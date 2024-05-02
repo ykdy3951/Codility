83e65476c3df5f15872bcdac608b279bfc5d5a86
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-05-03 01:40:02 +0900

    Task Score: 100% | Correctness Score: 100%

--- MaxCounters/MaxCounters.py
+++ MaxCounters/MaxCounters.py
@@ -5,11 +5,15 @@ def solution(N, A):
     # Implement your solution here
     l = [0] * N
     max_cnt = 0
+    update = 0
 
     for i in A:
-        if i == N + 1:
-            l = [max_cnt] * N
-        else:
-            l[i-1]+=1
+        if i != N + 1:
+            l[i-1] = max(update, l[i-1])+1
             max_cnt = max(max_cnt, l[i-1])
+        else:
+            update = max_cnt
+    for i in range(N):
+        if l[i] < update:
+            l[i] = update
     return l
