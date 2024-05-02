e9a4ded17870e90e1a7379b8aa76cb06b9eef443
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-05-03 01:13:16 +0900

    Task Score: 72% | Correctness Score: 50%

---
+++ FrogRiverOne/FrogRiverOne.py
@@ -0,0 +1,13 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(X, A):
+    # Implement your solution here
+    chk = [0] * X
+    res = 0
+    for i in range(len(A)):
+        if not chk[A[i]-1]:
+            chk[A[i]-1] = 1
+            res += 1
+        if res == X:
+            return i
