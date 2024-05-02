9c2324a94d427cc099cde08efadfdbedc1e24e57
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-05-03 00:38:08 +0900

    Task Score: 100% | Correctness Score: 100%

---
+++ FrogJmp/FrogJmp.py
@@ -0,0 +1,6 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+from math import ceil
+def solution(X, Y, D):
+    # Implement your solution here
+    return ceil((Y - X) / D)
