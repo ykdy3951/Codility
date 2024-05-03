b71739088fd9c072366a5f6bf2e105b9f73e5af7
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-05-03 13:52:04 +0900

    Task Score: 100% | Correctness Score: 100%

--- Triangle/Triangle.py
+++ Triangle/Triangle.py
@@ -4,13 +4,11 @@
 def solution(A):
     # Implement your solution here
     A.sort()
-    for i in range(len(A)):
-        if A[i] > 0:
-            A = A[i:]
-            break
-    
+
     for i in range(len(A)-2):
-        for j in range(i+1,len(A)-1):
+        if A[i] <= 0:
+            continue
+        for j in range(i+1, len(A)-1):
             if A[i] + A[j] > A[j+1]:
                 return 1
     return 0
